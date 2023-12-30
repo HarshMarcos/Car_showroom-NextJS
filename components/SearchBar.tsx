d592be6a9b764a3db7b69f45fc52622d7d2e23ea
admin <harshkurulkar7@gmail.com>
2023-12-30 16:51:02 +0530

    Add CardCard

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+
+import SearchManufacturer from "./SearchManufacturer";
+import { useState } from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form action="" className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
